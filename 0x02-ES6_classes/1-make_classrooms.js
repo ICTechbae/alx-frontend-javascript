30e6f4963cfb31ceb0d32282c434e09d395fecb4
ICTECHBAE <iwuohachioma30@gmail.com>
2024-04-25 09:16:23 -0700

    Working with class

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-	  const sizes = [19, 20, 34];
+  const sizes = [19, 20, 34];
 
-	  return sizes.map((size) => new ClassRoom(size));
+  return sizes.map((size) => new ClassRoom(size));
 }
